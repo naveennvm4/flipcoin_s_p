00ce6fd352c56620454e14fdc3781bcc6ffa3465
naveennvm4 <naveennvm4@gmail.com>
2021-01-22 17:34:01 +0530

    To display Heads or Tails

---
+++ flipcoin_s_p.sh
@@ -0,0 +1,40 @@
+spots=2
+coin=0
+if [ coin=$RANDOM%$spots+1 ]
+then
+    echo "heads"
+else
+    echo "tales"
+fi
+
+function coinFlip() 
+{
+rand=$(($RANDOM%2))
+hWin=0
+tWin=0
+if [ $rand -eq 0 ]
+then
+	echo "Heads Wins"
+	hwin=$(($hwin + 1))
+	echo $hwin
+else
+	echo "Tails Wins"
+	twin=$(($twin+1))
+	echo $twin
+fi
+}
+
+function coinFlipSimulation() 
+{
+echo "Enter number of times to execute"
+read n
+for (( i=0 ; $i < n ; i++ ))
+do
+	if [ $hwin -eq 21  -o  $twin -eq 21 ] 
+	then
+		break
+	else
+	     coinFlip
+	fi
+done
+}
